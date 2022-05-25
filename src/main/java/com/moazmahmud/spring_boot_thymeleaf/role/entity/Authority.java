ff5c7a0d892bfcbe623413ed4ad77ffa3052daaa
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2022-05-25 11:03:32 +0600

    added app user, role, authority entities

---
+++ src/main/java/com/moazmahmud/spring_boot_thymeleaf/role/entity/Authority.java
@@ -0,0 +1,36 @@
+package com.moazmahmud.spring_boot_thymeleaf.role.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "authority")
+public class Authority {
+    @Id
+    @SequenceGenerator(name = "seq_authority", sequenceName = "seq_authority")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_authority")
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Authority authority = (Authority) o;
+        return id != null && Objects.equals(id, authority.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ src/main/resources/db/changelog/data/user_role_authority_ddl.sql
@@ -0,0 +1,67 @@
+create table app_user
+(
+    id         int8 not null,
+    is_enabled boolean,
+    username   varchar(255),
+    primary key (id)
+);
+
+
+create table app_user_roles
+(
+    app_user_id int8 not null,
+    roles_id    int8 not null,
+    primary key (app_user_id, roles_id)
+);
+
+
+create table authority
+(
+    id   int8 not null,
+    name varchar(255),
+    primary key (id)
+);
+
+
+create table role
+(
+    id   int8 not null,
+    name varchar(255),
+    primary key (id)
+);
+
+
+create table role_authorities
+(
+    role_id        int8 not null,
+    authorities_id int8 not null,
+    primary key (role_id, authorities_id)
+);
+
+create sequence seq_app_user start 1 increment 50;
+create sequence seq_authority start 1 increment 50;
+create sequence seq_role start 1 increment 50;
+
+
+alter table app_user_roles
+    add constraint fk_roles_id
+        foreign key (roles_id)
+            references role;
+
+
+alter table app_user_roles
+    add constraint fk_app_user_id
+        foreign key (app_user_id)
+            references app_user;
+
+
+alter table role_authorities
+    add constraint fk_authorities_id
+        foreign key (authorities_id)
+            references authority;
+
+
+alter table role_authorities
+    add constraint fk_role_id
+        foreign key (role_id)
+            references role;
