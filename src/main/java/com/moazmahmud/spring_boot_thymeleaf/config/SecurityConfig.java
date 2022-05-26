8a0e37f81a8ce6478d9e8915af17a9323c6f1f7c
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2022-05-26 17:11:40 +0600

    added ant matchers

--- src/main/java/com/moazmahmud/spring_boot_thymeleaf/config/SecurityConfig.java
+++ src/main/java/com/moazmahmud/spring_boot_thymeleaf/config/SecurityConfig.java
@@ -4,12 +4,15 @@ import com.moazmahmud.spring_boot_thymeleaf.app_user.service.AppUserDetailsServi
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableWebSecurity
@@ -40,22 +43,27 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .cors()
-                .and()
-                .csrf()
-                .disable()
-                .sessionManagement()
-                .and()
-                .authorizeHttpRequests()
-                .antMatchers("/health-data/**").hasAnyRole("ADMIN")
-                .antMatchers(permitAllURL).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .headers()
-                .xssProtection();
+        http.cors()
+            .and()
+            .csrf()
+            .disable()
+            .sessionManagement()
+            .and()
+            .authorizeHttpRequests()
+            .antMatchers(GET,"/health-data").hasAnyAuthority("HEALTH_DATA_ADD")
+            .antMatchers(POST,"api/v1/health-data").hasAnyAuthority("HEALTH_DATA_ADD")
+            .antMatchers(GET,"/health-data/search", "/api/v1/health-data").hasAnyAuthority("HEALTH_DATA_VIEW", "HEALTH_DATA_EDIT", "HEALTH_DATA_DELETE")
+            .antMatchers(GET,"api/v1/health-data/raw-data").hasAnyAuthority("HEALTH_DATA_VIEW")
+            .antMatchers(GET,"/health-data/*").hasAnyAuthority("HEALTH_DATA_EDIT")
+            .antMatchers(POST,"api/v1/health-data/*").hasAnyAuthority("HEALTH_DATA_EDIT")
+            .antMatchers(DELETE,"/api/v1/health-data/*").hasAnyAuthority("HEALTH_DATA_DELETE")
+            .antMatchers(permitAllURL).permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .and()
+            .headers()
+            .xssProtection();
         return http.build();
     }
 }
