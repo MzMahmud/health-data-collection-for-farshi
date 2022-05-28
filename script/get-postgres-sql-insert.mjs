96b53d20f9de6baea2a6a7263fa7ff39fc982b1d
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2022-05-28 10:18:02 +0600

    refactored postgrace script

--- script/get-postgres-sql-insert.mjs
+++ script/get-postgres-sql-insert.mjs
@@ -1,11 +1,5 @@
 import fs from 'fs';
 
-let rawdata = fs.readFileSync('rakin_city_mirpur_13_20_may_2022.json');
-let dataList = JSON.parse(rawdata);
-
-console.log(dataList.length);
-
-
 function genderConverter(genderStr) {
     switch (genderStr) {
         case 'male': return `'MALE'`;
@@ -59,12 +53,20 @@ function getSqlInsertQuery(data, index) {
     const dataInsertionTime = new Date(data.dataInsertionTime);
     const insertTime = dataInsertionTime.toLocaleString();
     const eventDate = dataInsertionTime.toISOString().split('T')[0];
-    const locationName = 'Rakin City, Mirpur - 13';
     return `
 INSERT INTO health_data (id, created_at, is_deleted, modified_at, age, blood_pressure_diastolic_in_mm_hg, blood_pressure_systolic_in_mm_hg, blood_sugar_in_milli_mole_per_l, carb_intake_frequency, cereal_quality, event_date, gender, height_in_inch, location_name, physical_activity, religion, weight_in_kg) 
-VALUES (${index + 1}, '${insertTime}', false, '${insertTime}', ${data.age}, ${data.bloodPressureLow}, ${data.bloodPressureHigh}, ${data.bloodSugar}, ${carbIntakeFrequencyConverter(data.carbIntakeFrequency)}, ${cerealQualityConverter(data.cerealQuality)}, '${eventDate}', ${genderConverter(data.gender)}, ${data.height}, '${locationName}', ${physicalActivityConverter(data.physicalActivity)}, ${religionConverter(data.religion)}, ${data.weight});
+VALUES (${lastIndex + index + 1}, '${insertTime}', false, '${insertTime}', ${data.age}, ${data.bloodPressureLow}, ${data.bloodPressureHigh}, ${data.bloodSugar}, ${carbIntakeFrequencyConverter(data.carbIntakeFrequency)}, ${cerealQualityConverter(data.cerealQuality)}, '${eventDate}', ${genderConverter(data.gender)}, ${data.height}, '${locationName}', ${physicalActivityConverter(data.physicalActivity)}, ${religionConverter(data.religion)}, ${data.weight});
         `.trim();
 }
+
+
+
+const locationName = 'Rakin City, Mirpur-13';
+const lastIndex = 0;
+let rawdata = fs.readFileSync(`${locationName}.json`);
+let dataList = JSON.parse(rawdata);
+
+console.log(dataList.length);
 
 const sqlString =
     dataList
@@ -72,4 +74,4 @@ const sqlString =
         .join('\n')
         .trim();
 
-fs.writeFileSync('rakin_city_mirpur_data.sql', sqlString);
+fs.writeFileSync(`${locationName}.sql`, sqlString);
